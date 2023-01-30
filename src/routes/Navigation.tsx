f9009e597a625205e898b1afd8cf8c9ee411456c
Roberto Arenas <roberto.arenas2@icloud.com>
2023-01-29 20:15:26 -0600

    class name active in each link added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from 'react-router-dom';
 import logo from '../assets/react.svg';
 
 export const Navigation = () => {
@@ -9,20 +15,36 @@ export const Navigation = () => {
           <img src={logo} alt="React Logo" width="100%" className="pt" />
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users">User</NavLink>
+              <NavLink
+                to="/users"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                User
+              </NavLink>
             </li>
           </ul>
         </nav>
         <Routes>
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
-          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="home" element={<h1>Home Page</h1>} />
+          <Route path="/*" element={<Navigate to="home" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
